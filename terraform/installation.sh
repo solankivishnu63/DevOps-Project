887a3d19a26c2fa78c164b91a6cfc3197a34ca4d
solankivishnu63 <vishnusoul20052@gmail.com>
2026-01-29 17:32:38 +0530

    jenkins in script

--- terraform/installation.sh
+++ terraform/installation.sh
@@ -36,11 +36,21 @@ chmod +x /usr/local/bin/kind
 # Install Jenkins
 #################################
 sudo apt update
-sudo apt install -y openjdk-21-jre
-sudo  wget https://pkg.jenkins.io/debian-stable/binary/jenkins_2.452.4_all.deb
-sudo apt install ./jenkins_2.452.4_all.deb -y
-sudo systemctl enable jenkins
-sudo systemctl start jenkins
+sudo apt install fontconfig openjdk-21-jre
+java -version
+# this should be the result of the command above:
+#openjdk 21.0.8 2025-07-15
+#OpenJDK Runtime Environment (build 21.0.8+9-Debian-1)
+#OpenJDK 64-Bit Server VM (build 21.0.8+9-Debian-1, mixed mode, sharing)
+
+#Once you get this result then you can proceed with Jenkins installation
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update
+sudo apt install jenkins
 #################################
 
 # Kubectl installation
